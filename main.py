68a364e5a7febdd1ee9e590484a1f1c86f8f65fe
owen <owen@dice.fm>
2018-04-04 12:21:58 +0100

    play norah jones if too loud

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import soco
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger('conflict resolution')
 
+NORAH_JONES = "https://open.spotify.com/track/6ybViy2qrO9sIi41EgRJgx?si=BOMAiqYwSUyKxN0WTbKP8g"
 
 DISCOVERED = soco.discover()
 
@@ -16,10 +17,16 @@ def set_volume(args):
         zone.volume = args.volume
         logger.info("%s's volume set to: %s" % (zone.player_name, args.volume))
 
+ def check_volume():
+ 	for zone in DISCOVERED:
+ 		if zone.volume > 50:
+ 			zone.play_uri(NORAH_JONES)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--volume', default=5, help='sonos volume')
     args = parser.parse_args()
     logger.info("Yes, open offices sucks")
+    check_volume()
     set_volume(args)
